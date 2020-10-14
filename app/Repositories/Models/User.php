70382541243623e27a8614ac541e8797e6723631
Sam shi <plsxysam@gmail.com>
2020-10-14 16:04:33 +0800

    Initial commit

---
+++ app/Repositories/Models/User.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Jiannei/lumen-api-starter.
+ *
+ * (c) Jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Repositories\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
+{
+    use Authenticatable;
+    use Authorizable;
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Jiannei/lumen-api-starter.
+ *
+ * (c) Jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return app()->version();
+});
+
+Route::get('author', function () {
+    $response = Http::withOptions(['timeout' => 3])->get('https://api.github.com/users/Jiannei');
+    $response->throw();
+
+    return $response->json();
+});
+
+Route::get('configurations', 'ExampleController@configurations');
+Route::get('logs', 'ExampleController@logs');
+Route::get('enums', 'ExampleController@enums');
+Route::post('enums', 'ExampleController@enums');
+
+Route::post('users', 'UsersController@store');
+Route::get('users/{id}', 'UsersController@show');
+Route::get('users', 'UsersController@index');
+
+Route::post('authorization', 'AuthorizationController@store');
+Route::delete('authorization', 'AuthorizationController@destroy');
+Route::put('authorization', 'AuthorizationController@update');
+Route::get('authorization', 'AuthorizationController@show');
